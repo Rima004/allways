671145e0d9cc76140fdf558e1b660e21cd491664
refjs <daniel.perebinos@gmail.com>
2025-04-27 18:50:32 +0300

    Little refactor

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for our_app project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,16 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from main.views import *
-
+from apps.main.views import main_view, maps_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',main_view),
-    path('maps/',maps_view, name='maps'),
+    path("", main_view),
+    path("admin/", admin.site.urls),
+    path("maps/", maps_view, name="maps"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
